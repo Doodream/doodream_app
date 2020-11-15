4b1d78376b8d824857b40c59a3620aafb4d5fc34
doodream <rhomo17@naver.com>
2020-11-15 09:48:04 +0900

    LEARNING prop-types

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function Onepiece({ name, image }) {
+function Onepiece({ name, image, rating }) {
   return (
     <div>
       <h3>I love {name}</h3>
-      <img src={image} alt={name} />
+      <h4>{rating}/5.0</h4>
+      <img src={image} alt={name}></img>
     </div>
 
   );
+}
+
+Onepiece.propTypes = {
+  name: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired
 }
 
 const onepiece = [
@@ -26,24 +34,27 @@ const onepiece = [
   {
     id: 3,
     name: "나미",
-    image: "https://image.aladin.co.kr/product/21891/12/cover500/s972636400_1.jpg"
+    image: "https://image.aladin.co.kr/product/21891/12/cover500/s972636400_1.jpg",
+    rating: 4.7
   },
   {
     id: 4,
     name: "우솝",
-    image: "https://image.aladin.co.kr/product/21891/12/cover500/s972636400_1.jpg"
+    image: "https://image.aladin.co.kr/product/21891/12/cover500/s972636400_1.jpg",
+    rating: 4.4
   },
   {
     id: 5,
     name: "버기",
-    image: "https://image.aladin.co.kr/product/21891/12/cover500/s972636400_1.jpg"
+    image: "https://image.aladin.co.kr/product/21891/12/cover500/s972636400_1.jpg",
+    rating: 4.3
   }
 ]
 
 
 function App() {
   return (
-    onepiece.map(name => <Onepiece key={name.id} name={name.name} image={name.image} />)
+    onepiece.map(name => <Onepiece key={name.id} name={name.name} image={name.image} rating = {name.rating}/>)
   )
 }
 
