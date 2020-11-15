c4fb3b66be942f10e5c663892d5438383b837746
doodream <rhomo17@naver.com>
2020-11-15 17:43:53 +0900

    LEARNING react  - life cycle method

--- src/App.js
+++ src/App.js
@@ -1,61 +1,42 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+//import PropTypes from 'prop-types';
 
-function Onepiece({ name, image, rating }) {
-  return (
-    <div>
-      <h3>I love {name}</h3>
-      <h4>{rating}/5.0</h4>
-      <img src={image} alt={name}></img>
-    </div>
 
-  );
-}
 
-Onepiece.propTypes = {
-  name: PropTypes.string.isRequired,
-  image: PropTypes.string.isRequired,
-  rating: PropTypes.number.isRequired
-}
-
-const onepiece = [
-  {
-    id: 1,
-    name: "루피",
-    image: "https://image.aladin.co.kr/product/21891/12/cover500/s972636400_1.jpg",
-    rating: 5
-  },
-  {
-    id: 2,
-    name: "상디",
-    image: "https://image.aladin.co.kr/product/21891/12/cover500/s972636400_1.jpg",
-    rating: 4.9
-  },
-  {
-    id: 3,
-    name: "나미",
-    image: "https://image.aladin.co.kr/product/21891/12/cover500/s972636400_1.jpg",
-    rating: 4.7
-  },
-  {
-    id: 4,
-    name: "우솝",
-    image: "https://image.aladin.co.kr/product/21891/12/cover500/s972636400_1.jpg",
-    rating: 4.4
-  },
-  {
-    id: 5,
-    name: "버기",
-    image: "https://image.aladin.co.kr/product/21891/12/cover500/s972636400_1.jpg",
-    rating: 4.3
+class App extends React.Component {
+  state = {
+    count: 0
   }
-]
+  constructor(props) {
+    super(props);
+    console.log("im constructor");
+  }
+  add = () => {
+    this.setState(a => ({ count: a.count + 1 }));
+  };
 
-
-function App() {
-  return (
-    onepiece.map(name => <Onepiece key={name.id} name={name.name} image={name.image} rating = {name.rating}/>)
-  )
+  minus = () => {
+    this.setState(current => ({ count: current.count - 1 }));
+  };
+  componentDidMount() {
+    console.log("im componentDidMount");
+  }
+  componentDidUpdate() {
+    console.log("Hey iam updated");
+  }
+  componentWillUnmount() {
+    console.log("Goodbye component");
+  }
+  render() {
+    console.log("im rendering");
+    return (
+      <div>
+        <h1>This number is {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    )
+  }
 }
 
 export default App;
