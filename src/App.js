77cca29192b026bae57bf6959e8ff9cd49aa5169
doodream <rhomo17@naver.com>
2020-11-15 23:57:48 +0900

    LEARING react - async,await / setTimeout()

--- src/App.js
+++ src/App.js
@@ -1,41 +1,43 @@
 import React from 'react';
+import axios from 'axios';
+import Movie from './Movie';
 //import PropTypes from 'prop-types';
 
 
 
 class App extends React.Component {
   state = {
-    count: 0
-  }
-  constructor(props) {
-    super(props);
-    console.log("im constructor");
-  }
-  add = () => {
-    this.setState(a => ({ count: a.count + 1 }));
+    isLoading: true,
+    movies: []
   };
+  // render하면 진행되는 함수이다. 이함수는 setTimeout()함수를 통해 setState()를 6초 후에 시켰다.
+  // 즉 data를 fetch 한것이다.
 
-  minus = () => {
-    this.setState(current => ({ count: current.count - 1 }));
-  };
+  getMovies = async () => {
+    const {
+      data: {
+        data: { movies }
+      }
+    } = await axios.get("https://yts.mx/api/v2/list_movies.json?sort_by=rating");
+    this.setState({ movies, isLoading: false });
+  }
   componentDidMount() {
-    console.log("im componentDidMount");
+    this.getMovies();
   }
-  componentDidUpdate() {
-    console.log("Hey iam updated");
-  }
-  componentWillUnmount() {
-    console.log("Goodbye component");
-  }
+
   render() {
-    console.log("im rendering");
+    const { isLoading, movies } = this.state;
     return (
-      <div>
-        <h1>This number is {this.state.count}</h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minus</button>
-      </div>
-    )
+      <div>{isLoading ? "Loading..." : movies.map(movie => {
+        return <Movie
+          key={movie.id}
+          id={movie.id}
+          title={movie.title}
+          summary={movie.summary}
+          poster={movie.medium_cover_image}
+          year={movie.year}></Movie>
+      })}</div>
+    );
   }
 }
 
